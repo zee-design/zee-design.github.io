bb9eb2ae7c90acd28745d633c751c2ee8119ce9e
07Zee07 <51439896+07Zee07@users.noreply.github.com>
2020-07-02 15:15:38 +0100

    Add files via upload

---
+++ js/share_b.js
@@ -0,0 +1,21 @@
+const shareButton = document.querySelector('.share-button');
+const shareDialog = document.querySelector('.share-dialog');
+const closeButton = document.querySelector('.close-button');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'Share Portfolio',
+      url: 'https://07zee07.github.io'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    } else {
+        shareDialog.classList.add('is-open');
+    }
+});
+
+closeButton.addEventListener('click', event => {
+  shareDialog.classList.remove('is-open');
+});
